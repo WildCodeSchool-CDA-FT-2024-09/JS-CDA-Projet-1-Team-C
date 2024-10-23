9affd1cbb50749cca8a6725406f7eab8dd106b13
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-10-23 16:47:11 +0200

    ajout d'une mutation pour ajouter des équipes dans le Team resolver

--- api/src/team/team.resolver.ts
+++ api/src/team/team.resolver.ts
@@ -1,10 +1,34 @@
 import { Team } from "./team.entity";
-import { Resolver, Query } from "type-graphql";
+import { Resolver, Query, InputType, Field, Mutation, Arg } from "type-graphql";
+
+@InputType()
+class TeamInput implements Partial<Team> {
+  @Field()
+  name: string;
+
+  @Field()
+  location: string;
+
+  @Field()
+  contact: string;
+}
 
 @Resolver(Team)
 export default class TeamResolver {
   @Query(() => [Team])
-  async fullteam() {
-    return [];
+  async allTeams() {
+    return await Team.find();
+  }
+
+  @Mutation(() => Team)
+  async create(@Arg("team") newTeam: TeamInput) {
+    const teamToInsert = new Team();
+
+    teamToInsert.name = newTeam.name;
+    teamToInsert.location = newTeam.location;
+    teamToInsert.contact = newTeam.contact;
+
+    const result = await teamToInsert.save();
+    return result;
   }
 }
