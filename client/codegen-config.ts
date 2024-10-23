2de67a3ab30ddd4e9a23084627d25d077ecd2223
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-10-23 12:33:30 +0200

    installation de dotenv en dépendance de dev pour le front de manière à avoir accès aux variables d'environeement pour la config de codegen

--- client/codegen-config.ts
+++ client/codegen-config.ts
@@ -1,8 +1,12 @@
 import { CodegenConfig } from "@graphql-codegen/cli";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+const { VITE_API_URL } = process.env;
 
 const config: CodegenConfig = {
   /* @ts-expect-error:outside the scope */
-  schema: "http://localhost:4000",
+  schema: VITE_API_URL,
   documents: ["src/schemas/*.ts"],
   generates: {
     "./src/types/graphql-types.ts": {
