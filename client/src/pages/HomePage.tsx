3fd17076156fa16c18aa28b45d13e43e2525ac3b
JohaneDecamps <johane.decamps@gmail.com>
2024-10-24 10:14:00 +0200

    Changement du bouton en MUILink

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,23 +1,10 @@
-import Link from "@mui/material/Link";
+import { Link as RouterLink } from "react-router-dom";
+import { Link as MUILink, Typography } from "@mui/material";
 import Grid from "@mui/material/Grid2";
-import Button, { ButtonProps } from "@mui/material/Button";
-import { styled } from "@mui/material/styles";
-import BarChartIcon from "@mui/icons-material/BarChart";
-import { red } from "@mui/material/colors";
-import { green } from "@mui/material/colors";
 import Paper from "@mui/material/Paper";
-
-import imgFirst from "../../assets/images/pexels-vanessa-loring-7868838.jpg"
-import imgSnd from "../../assets/images/pexels-vanessa-loring-7868890.jpg"
-
-const ColorButtonRed = styled(Button)<ButtonProps>(({ theme }) => ({
-  color: theme.palette.getContrastText(red[500]),
-  backgroundColor: red[500],
-}));
-
-const ColorButtonGreen = styled(Button)<ButtonProps>(() => ({
-  backgroundColor: green[500],
-}));
+import { styled } from "@mui/material/styles";
+import imgFirst from "../../assets/images/pexels-vanessa-loring-7868838.jpg";
+import imgSnd from "../../assets/images/pexels-vanessa-loring-7868890.jpg";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: "#fff",
@@ -30,15 +17,21 @@ const Item = styled(Paper)(({ theme }) => ({
   }),
 }));
 
+const buttonStyle = {
+  padding: "8px 16px",
+  margin: "10px",
+  color: "black",
+  backgroundColor: "primary.main",
+  borderRadius: "4px",
+  textDecoration: "none",
+  width: "20%",
+  textAlign: "center",
+};
+
 export default function HomePage() {
   return (
     <>
-      <Grid
-        container
-        spacing={5}
-        justifyContent="center"
-        sx={{ marginTop: 9 }}
-      >
+      <Grid container spacing={5} justifyContent="center" sx={{ marginTop: 9 }}>
         <Grid size={5}>
           <img src={imgSnd} style={{ width: "100%", height: "auto" }} />
         </Grid>
@@ -67,7 +60,7 @@ export default function HomePage() {
             numquam laborum pariatur ab obcaecati! Nam, animi? Labore optio
             possimus earum asperiores error placeat itaque perferendis pariatur
             accusamus. Voluptas numquam laborum pariatur ab obcaecati! Nam,
-            animi?
+            animi? 
           </Item>
         </Grid>
         <Grid size={5}>
@@ -75,38 +68,39 @@ export default function HomePage() {
         </Grid>
       </Grid>
 
-      <Grid container justifyContent="center">
-        <Link href="/ranking">
-          <ColorButtonGreen
-            variant="contained"
-            endIcon={<BarChartIcon />}
-            sx={{ marginTop: 9 }}
-          >
-            Voir les classements
-          </ColorButtonGreen>{" "}
-        </Link>
+      <Grid container justifyContent="center" marginTop="5vh">
+        <MUILink
+          component={RouterLink}
+          to="/raking"
+          sx={buttonStyle}
+        >
+        <Typography variant="h6">
+        Voir les classements
+        </Typography>
+        </MUILink>{" "}
       </Grid>
       <Grid container justifyContent="center">
-        <Link href="/ranking">
-          <Button
-            variant="contained"
-            endIcon={<BarChartIcon />}
-            sx={{ marginTop: 5 }}
-          >
-            Voir les classements
-          </Button>{" "}
-        </Link>
+        <MUILink
+          component={RouterLink}
+           to="/raking"
+          sx={buttonStyle}
+        >
+        
+        <Typography variant="h6">
+        Voir les classements
+        </Typography>
+        </MUILink>{" "}
       </Grid>
       <Grid container justifyContent="center">
-        <Link href="/ranking">
-          <ColorButtonRed
-            variant="contained"
-            endIcon={<BarChartIcon />}
-            sx={{ marginTop: 5 }}
-          >
-            Voir les classements
-          </ColorButtonRed>{" "}
-        </Link>
+        <MUILink
+          component={RouterLink}
+           to="/raking"
+          sx={buttonStyle}
+        >
+        <Typography variant="h6">
+        Voir les classements
+        </Typography>
+        </MUILink>{" "}
       </Grid>
     </>
   );
