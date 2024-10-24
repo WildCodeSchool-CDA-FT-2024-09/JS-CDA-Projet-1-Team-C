f8cab07fe859792d567d5dbdc102fb403139876a
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-10-24 18:30:12 +0200

    func(api) : créationde la mutation edit pour les teams

--- api/src/team/team.resolver.ts
+++ api/src/team/team.resolver.ts
@@ -1,9 +1,14 @@
 import { Team } from "./team.entity";
 import { Resolver, Query, InputType, Field, Mutation, Arg } from "type-graphql";
-import { IsString, Length } from "class-validator";
+import { IsNumber, IsString, IsOptional, Length } from "class-validator";
 
 @InputType()
 class TeamInput implements Partial<Team> {
+  @Field()
+  @IsNumber()
+  @IsOptional()
+  id?: number;
+
   @Field()
   @IsString()
   @Length(3, 100)
@@ -37,5 +42,26 @@ export default class TeamResolver {
 
     const result = await teamToInsert.save();
     return result;
+  }
+
+  @Mutation(() => Team)
+  async edit(@Arg("team") newTeam: TeamInput) {
+    try {
+      const teamToEdit = await Team.findOneBy({ id: newTeam.id });
+
+      if (!teamToEdit) {
+        throw new Error(`Team with ID ${newTeam.id} not found`);
+      } else {
+        teamToEdit.name = newTeam.name;
+        teamToEdit.location = newTeam.location;
+        teamToEdit.contact = newTeam.contact;
+
+        const result = await teamToEdit.save();
+        return result;
+      }
+    } catch (error) {
+      console.error(error);
+      throw new Error("Failed to edit team");
+    }
   }
 }
