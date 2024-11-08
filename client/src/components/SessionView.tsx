2f62091051b822b43e64b3b0e5735bc070b47169
scyanth <34488277+scyanth@users.noreply.github.com>
2024-11-08 14:22:01 +0100

    finalisation juries planning

--- client/src/components/SessionView.tsx
+++ client/src/components/SessionView.tsx
@@ -3,26 +3,28 @@ import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 
 type Session = {
-  id: number;
-  start: string;
-  end: string;
-  competition: { id: number; name: string };
-  team: { id: number; name: string };
+  // id: number;
+  startTime: string;
+  endTime: string;
+  teamName: string;
+  // competition: { id: number; name: string };
+  // team: { id: number; name: string };
 };
 
-type SessionViewProps = {
-  session: Session;
-};
+// type SessionViewProps = {
+//   session: Session;
+// };
 
-export default function SessionView({ session }: SessionViewProps) {
+export default function SessionView({ startTime, endTime, teamName }: Session) {
+  //{ session }: SessionViewProps
   return (
     <Card sx={{ maxWidth: 250 }}>
       <CardContent>
         <Typography gutterBottom sx={{ fontSize: 14, fontWeight: "bold" }}>
-          Equipe : {session.team.name}
+          Equipe : {teamName}
         </Typography>
-        <Typography variant="body2"> Commence à : {session.start}</Typography>
-        <Typography variant="body2"> Termine à : {session.end}</Typography>
+        <Typography variant="body2"> Commence à : {startTime}</Typography>
+        <Typography variant="body2"> Termine à : {endTime}</Typography>
       </CardContent>
     </Card>
   );
--- client/src/pages/Juries.tsx
+++ client/src/pages/Juries.tsx
@@ -1,89 +1,37 @@
-// import * as React from "react";
-// import { useEffect } from "react";
+import * as React from "react";
 import Grid from "@mui/material/Grid2";
 import Typography from "@mui/material/Typography";
-import Select from "@mui/material/Select";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
 import Box from "@mui/material/Box";
-// import MenuItem from "@mui/material/MenuItem";
-import SessionView from "../components/SessionView";
-import sessions from "../../assets/sessions.json";
-// import { useGetAllCompetitionsQuery } from "../types/graphql-types";
-// import { useGetAllJuriesQuery } from "../types/graphql-types";
-// import { useGetJuriesByUserQuery } from "../types/graphql-types";
-// import { Jury } from "../types/graphql-types";
+import MenuItem from "@mui/material/MenuItem";
 import InputLabel from "@mui/material/InputLabel";
 import FormControl from "@mui/material/FormControl";
-
-// type juryType = {
-//   id: number;
-//   name: string;
-//   users: {
-//     id: number;
-//   };
-//   competitions: {
-//     id: number;
-//   };
-// };
-
-// type competitionType = {
-//   id: number;
-//   name: string;
-//   date: string;
-// };
+import SessionView from "../components/SessionView";
+import { useGetJuriesByUserQuery } from "../types/graphql-types";
 
 export default function Juries() {
-  // const jurorId = 1;
+  const userId = 2;
 
-  // const [juryInput, setJuryInput] = React.useState("");
-  // const [juryId, setJuryId] = React.useState<number>(0);
-  // //const [competition, setCompetition] = React.useState<
-  //   competitionType | undefined
-  // >();
+  const [juryId, setJuryId] = React.useState<number>(1);
 
-  // const {
-  //   loading: loadingCompetitions,
-  //   error: errorCompetitions,
-  //   data: competitionsData,
-  // } = useGetAllCompetitionsQuery();
-  // const {
-  //   loading: loadingJuries,
-  //   error: errorJuries,
-  //   data: juriesData,
-  // } = useGetJuriesByUserQuery();
+  const {
+    loading: loadingJuries,
+    error: errorJuries,
+    data: juriesData,
+  } = useGetJuriesByUserQuery({
+    variables: { userId: userId },
+  });
 
-  // if (loadingCompetitions) return <p>Loading...</p>;
-  // if (errorCompetitions) return <p>Error :(</p>;
+  const juries = juriesData?.getJuriesByUser;
+  let jury = juries?.find((jury) => jury.id === juryId);
 
-  // const juries = juriesData?.getAllJuries.filter((jury) =>
-  //   jury.users.find((user) => user.id === jurorId)
-  // );
+  if (loadingJuries) return <p>Loading...</p>;
+  if (errorJuries) return <p>Error :(</p>;
 
-  // useEffect(() => {
-  //   if (!competition) {
-  //     const comp = juries?.find((jury) => jury.id === juryId)?.competition;
-  //     setCompetition(comp);
-  //   }
-  // }, [juries, juryId, competition]);
-
-  // console.info("juré 1:", jurorId);
-  // console.info("jurys avec le juré 1 :", juries);
-  // console.info(
-  //   "compétition du jury 1er : ",
-  //   juries && juries![0].competition.name
-  // );
-
-  //competition = competitionsData.getAllCompetitions.filter((competition) => (competition === jury.competition));
-  // const handleChange = (event: SelectChangeEvent) => {
-  //   //const test = event.target.value as Number;
-  //   // const selected =
-  //   //   juries && juries.find((jury) => jury.id === parseInt(event.target.value));
-  //   setJuryId(parseInt(event.target.value));
-  //   const jury = juries && juries.find((jury) => jury.id === juryId);
-  //   setCompetition(jury && jury.competition);
-  //   // const competitionName = jury && jury.competition.name;
-  //   // const competitionDate = jury && jury.competition.date;
-  //   // setCompetition(jury.competition);
-  // };
+  const handleChange = (event: SelectChangeEvent) => {
+    setJuryId(parseInt(event.target.value));
+    jury = juries?.find((jury) => jury.id === juryId);
+  };
 
   return (
     <>
@@ -97,30 +45,34 @@ export default function Juries() {
         <Typography variant="h2" component="h1">
           Mon planning
         </Typography>
-        <Typography variant="h3" component="h1">
-          Pour la compétition la date
-        </Typography>
       </Box>
+      <Typography variant="h4" component="h1" sx={{ marginBottom: 6 }}>
+        Pour la compétition {jury?.competition.name} du {jury?.competition.date}
+      </Typography>
       <FormControl sx={{ width: 300 }}>
-        <InputLabel id="jury-select-label">Jurys</InputLabel>
+        <InputLabel id="demo-simple-select-label">Jurys</InputLabel>
         <Select
-          labelId="jury-select-label"
+          labelId="demo-simple-select-label"
           id="jury-select"
-          value={""}
+          value={jury?.name}
           label="Jury"
-          // onChange={handleChange}
+          onChange={handleChange}
         >
-          {/* {juries &&
+          {juries &&
             juries.map((jury) => (
               <MenuItem value={jury.id}>{jury.name}</MenuItem>
-            ))} */}
+            ))}
         </Select>
       </FormControl>
 
       <Grid container spacing={2} sx={{ marginTop: 6 }}>
-        {sessions.map((sess) => (
+        {jury?.sessions.map((sess) => (
           <Grid size={3}>
-            <SessionView session={sess} />
+            <SessionView
+              startTime={sess.startTime}
+              endTime={sess.endTime}
+              teamName={sess.team.name}
+            />
           </Grid>
         ))}
       </Grid>
